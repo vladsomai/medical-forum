c31efb1b0532cb72283a8fa23a25e8bad8af1c4b
Vlad Somai <Vlad.Somai-3rdpty@brooks.com>
2023-03-01 22:20:09 +0200

    add appointment confirmation

---
+++ src/lib/components/modal/Appointment/appointmentDone.svelte
@@ -0,0 +1,16 @@
+<script>
+	import AppointmentDoneImg from '$lib/images/appointment-done.svg';
+</script>
+
+<div class="w-full h-full flex flex-col justify-center items-center">
+	<h3 class="text-3xl my-5">Succes!</h3>
+	<p class="text-justify my-5">
+		Am preluat programarea ta, in scurt timp te vom contacta telefonic pentru a confirma o data
+		disponibila.
+	</p>
+	<p>
+		Poti sa te preziti la clinica noastra doar in momentul in care iti confirmam acest lucru prin
+		email sau telefonic.
+	</p>
+	<img src={AppointmentDoneImg} class="h-3/6 w-3/6 mt-10" alt="email sent" />
+</div>
--- src/routes/appointment/+page.svelte
+++ src/routes/appointment/+page.svelte
@@ -2,12 +2,18 @@
 	import EmailIcon from '$lib/images/icons/email.svg';
 	import NameIcon from '$lib/images/icons/person-details.svg';
 	import PhoneIcon from '$lib/images/icons/telephone.svg';
-	import DatetimeIcon from '$lib/images/icons/appointment-datetime.svg';
 	import SpecialityIcon from '$lib/images/icons/appointment-speciality.svg';
 	import { Specialities } from '$lib/types';
+	import AppointmentDone from '$lib/components/modal/Appointment/appointmentDone.svelte';
+	import { modalContent } from '$lib/stores/globalStore';
 	let loading = false;
+	let modalElem = document.getElementById('modalElement');
 
-	function handleAppointment() {}
+	function handleAppointment() {
+		//@ts-ignore
+		$modalContent = AppointmentDone;
+		modalElem?.click();
+	}
 </script>
 
 <svelte:head>
@@ -15,84 +21,71 @@
 	<meta name="description" content="Medical Forum - Programare fara cont" />
 </svelte:head>
 
-<div class="mt-[15vh] mx-[10%] flex flex-col justify-center items-center">
-	<p class="text-justify my-5">
-		In cazul in care doresti sa iti faci o programare fara un cont, completeaza formularul de mai
-		jos iar noi vom reveni catre tine cu un apel telefonic pentru a confirma programarea.
-	</p>
-	<form class="flex flex-col w-[80%]" on:submit={handleAppointment}>
-		<div class="input input-primary mb-4 rounded-full flex justify-between items-center ">
-			<img src={NameIcon} alt="last name" />
-			<input
-				name="lastName"
-				required
-				placeholder="Nume"
-				class="input input-primary input-sm border-0 rounded-r-full focus:outline-none outline-none focus:bg-transparent bg-transparent active:bg-transparent ml-1"
-				type="text"
-			/>
-		</div>
-		<div class="input input-primary mb-4 rounded-full flex justify-between items-center ">
-			<img src={NameIcon} alt="first name" />
-			<input
-				name="firstName"
-				required
-				placeholder="Prenume"
-				class="input input-primary input-sm border-0 rounded-r-full focus:outline-none outline-none focus:bg-transparent bg-transparent active:bg-transparent ml-1"
-				type="text"
-			/>
-		</div>
-		<div class="input input-primary mb-4 rounded-full flex justify-between items-center ">
-			<img src={EmailIcon} alt="email" />
-			<input
-				name="email"
-				required
-				placeholder="Email"
-				class="input input-primary input-sm border-0 rounded-r-full focus:outline-none outline-none focus:bg-transparent bg-transparent active:bg-transparent ml-1"
-				type="email"
-			/>
-		</div>
-		<div class="input input-primary mb-4 rounded-full flex justify-between items-center ">
-			<img src={PhoneIcon} alt="phone" />
-			<input
-				name="phone"
-				required
-				placeholder="Numar de telefon"
-				class="input input-primary input-sm border-0 rounded-r-full focus:outline-none outline-none focus:bg-transparent bg-transparent active:bg-transparent ml-1"
-				type="tel"
-			/>
-		</div>
-		<div class="input input-primary mb-4 rounded-full flex justify-between items-center ">
-			<img src={SpecialityIcon} alt="appointment speciality" />
-			<select
-				class="select select-primary select-sm w-full max-w-xs border-0 rounded-r-full focus:outline-none outline-none focus:bg-transparent bg-transparent active:bg-transparent ml-1"
+<div class="w-full h-screen fixed top-0 flex justify-center items-center">
+	<div class=" mx-[10%] flex flex-col justify-center items-center">
+		<form class="flex flex-col my-5 " on:submit={handleAppointment}>
+			<div class="input input-primary mb-4 rounded-full flex justify-between items-center ">
+				<img src={NameIcon} alt="last name" />
+				<input
+					name="lastName"
+					required
+					placeholder="Nume"
+					class="input input-primary input-sm border-0 rounded-r-full focus:outline-none outline-none focus:bg-transparent bg-transparent active:bg-transparent ml-1"
+					type="text"
+				/>
+			</div>
+			<div class="input input-primary mb-4 rounded-full flex justify-between items-center ">
+				<img src={NameIcon} alt="first name" />
+				<input
+					name="firstName"
+					required
+					placeholder="Prenume"
+					class="input input-primary input-sm border-0 rounded-r-full focus:outline-none outline-none focus:bg-transparent bg-transparent active:bg-transparent ml-1"
+					type="text"
+				/>
+			</div>
+			<div class="input input-primary mb-4 rounded-full flex justify-between items-center ">
+				<img src={EmailIcon} alt="email" />
+				<input
+					name="email"
+					required
+					placeholder="Email"
+					class="input input-primary input-sm border-0 rounded-r-full focus:outline-none outline-none focus:bg-transparent bg-transparent active:bg-transparent ml-1"
+					type="email"
+				/>
+			</div>
+			<div class="input input-primary mb-4 rounded-full flex justify-between items-center ">
+				<img src={PhoneIcon} alt="phone" />
+				<input
+					name="phone"
+					required
+					placeholder="Numar de telefon"
+					class="input input-primary input-sm border-0 rounded-r-full focus:outline-none outline-none focus:bg-transparent bg-transparent active:bg-transparent ml-1"
+					type="tel"
+				/>
+			</div>
+			<div class="input input-primary mb-4 rounded-full flex justify-between items-center ">
+				<img src={SpecialityIcon} alt="appointment speciality" />
+				<select
+					class="select select-primary select-sm w-full max-w-xs border-0 rounded-r-full focus:outline-none outline-none focus:bg-transparent bg-transparent active:bg-transparent ml-1"
+				>
+					<option disabled selected>Specializare</option>
+					{#each [...Specialities] as [key, value]}
+						<option>{value}</option>
+					{/each}
+				</select>
+			</div>
+			<div class="form-control mb-4 flex justify-between items-center">
+				<label class="label cursor-pointer flex items-center justify-center">
+					<span class="label-text mr-3 text-[16px]">Ai nevoie si de investigatii?</span>
+					<input type="checkbox" checked={true} class="checkbox checkbox-primary" />
+				</label>
+			</div>
+			<button
+				class={`btn btn-primary rounded-full ${loading ? 'loading' : ''}`}
+				disabled={loading}
+				type="submit">Programeaza-te fara cont!</button
 			>
-				<option disabled selected>Specializare</option>
-                {#each [...Specialities] as [key,value] }
-                    <option>{value}</option>
-                {/each}
-			</select>
-		</div>
-		<div class="input input-primary mb-4 rounded-full flex justify-between items-center ">
-			<img src={DatetimeIcon} alt="appointment date" />
-			<input
-				name="date"
-				required
-				placeholder="Data"
-				class="input input-primary input-sm border-0 rounded-r-full focus:outline-none outline-none focus:bg-transparent bg-transparent active:bg-transparent ml-1"
-				type="datetime-local"
-			/>
-		</div>
-
-		<div class="form-control mb-4 flex justify-between items-center">
-			<label class="label cursor-pointer flex items-center justify-center">
-				<span class="label-text mr-3 text-[16px]">Ai nevoie si de investigatii?</span>
-				<input type="checkbox" checked={true} class="checkbox checkbox-primary" />
-			</label>
-		</div>
-		<button
-			class={`btn btn-primary rounded-full ${loading ? 'loading' : ''}`}
-			disabled={loading}
-			type="submit">Programeaza-te fara un cont!</button
-		>
-	</form>
+		</form>
+	</div>
 </div>
